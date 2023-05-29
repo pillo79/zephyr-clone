fd89fd0125e0f181bff04abee1954ce09e474e5a
Luca Burelli <l.burelli@arduino.cc>
2023-05-29 16:32:36 +0200

    wip test manual-init with uart4 (TX2 pin)

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -13,6 +13,12 @@
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
+/* change this to any other UART peripheral if desired */
+#define UART_DEVICE_NODE DT_NODELABEL(uart4)
+
+static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
+#include <zephyr/drivers/uart.h>
+
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
@@ -32,12 +38,26 @@ int main(void)
 		return 0;
 	}
 
+	ret = device_init(uart_dev);
+	if (ret < 0) {
+		printk("UART device init err %i", ret);
+		return 0;
+	}
+
+	if (!device_is_ready(uart_dev)) {
+		printk("UART device not found!");
+		return 0;
+	}
+
+	const char *msg="0123456789abcdef";
+	int i=0;
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
 			return 0;
 		}
 		k_msleep(SLEEP_TIME_MS);
+		uart_poll_out(uart_dev, msg[(i++) & 0xf]);
 	}
 	return 0;
 }
